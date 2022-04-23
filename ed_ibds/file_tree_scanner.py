85846900397e22dbb54153141862a7b6e7219c1b
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-04-23 17:19:48 +0300

    Use edpu.file_tree_walker

--- ed_ibds/file_tree_scanner.py
+++ ed_ibds/file_tree_scanner.py
@@ -1,4 +1,4 @@
-import os
+from edpu import file_tree_walker
 from . import standard_type_assertion
 from . import ibds_utils
 
@@ -7,20 +7,7 @@ def scan(root_path, skip_paths):
     standard_type_assertion.assert_string('root_path', root_path)
     standard_type_assertion.assert_list_pred('skip_paths', skip_paths, standard_type_assertion.assert_string)
 
-    if not os.path.isdir(root_path):
-        raise Exception(root_path + ' does not exist')
-
-    file_tree = []
-
-    for cur_root_path, _, files in os.walk(root_path):
-        rel_path_text = os.path.relpath(cur_root_path, root_path)
-        if rel_path_text == '.':
-            rel_path = []
-        else:
-            rel_path = rel_path_text.split(os.sep)
-
-        for cur_file in files:
-            if not ibds_utils.path_needs_skip(rel_path + [cur_file], skip_paths):
-                file_tree.append(rel_path + [cur_file])
-
-    return file_tree
+    return file_tree_walker.walk(
+        root_path,
+        lambda type, path: type == file_tree_walker.TYPE_DIR or ibds_utils.path_needs_skip(path, skip_paths)
+    ).get(file_tree_walker.TYPE_FILE)
