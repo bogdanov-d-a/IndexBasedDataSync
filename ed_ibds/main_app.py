0382746dbaa880170970facc4b87a93daf6feac8
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-01-27 03:32:09 +0600

    Spelling fix

--- ed_ibds/main_app.py
+++ ed_ibds/main_app.py
@@ -43,4 +43,4 @@ def run(user_data):
         else:
             raise Exception('unexpected action')
 
-    pause_at_end.run(main, 'Program finished successfully')
+    pause_at_end.run(main, 'Program completed successfully')
